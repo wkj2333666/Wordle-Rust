e19e3d63ef74aa185a94eb3a829b487d2767b1b3
wkj2333666 <2695071759@qq.com>
2025-08-24 21:18:34 +0800

    refactor: put wordle-cli in lib.rs, main.rs is only an entry

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use clap::Parser;
+use std::io;
+mod args;
+use args::Args;
+
+use crate::game::init_game;
+mod builtin_words;
+mod game;
+mod recorder;
+
+pub struct Wordle {
+    is_tty: bool,
+    args: Args,
+    game_recorder: recorder::GameRecorder,
+    game_data: recorder::GameData,
+}
+
+impl Wordle {
+    pub fn new() -> Self {
+        Wordle {
+            is_tty: atty::is(atty::Stream::Stdout),
+            args: Args::parse(),
+            game_recorder: recorder::GameRecorder::new(),
+            game_data: recorder::GameData::new(),
+        }
+    }
+
+    fn game_loop(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut final_words = Vec::<String>::new();
+        let mut acceptable = Vec::<String>::new();
+        init_game(&self.args, &mut final_words, &mut acceptable)?;
+
+        loop {
+            game::start_one_game(
+                self.is_tty,
+                &self.args,
+                &mut self.game_recorder,
+                &final_words,
+                &acceptable,
+                &mut self.game_data,
+            );
+            // Show stats if requested
+            if self.args.stats {
+                self.game_recorder.print();
+            }
+
+            // Day++
+            if self.args.day.is_some() {
+                self.args.day = Some(self.args.day.unwrap() + 1);
+            }
+
+            // Save game data if requested
+            if self.args.state.is_some() {
+                self.game_data.save(&self.args)?;
+            }
+
+            // Do not play again if word is specified
+            if self.args.word.is_some() {
+                break;
+            }
+
+            let play_again = loop {
+                let mut buf = String::new();
+                let is_eof = io::stdin().read_line(&mut buf).expect("Invalid input!");
+                if is_eof == 0 {
+                    break false;
+                }
+                buf = buf.trim().to_string();
+                if buf == "Y" {
+                    break true;
+                } else if buf == "N" {
+                    break false;
+                }
+                println!("Invalid input!");
+            };
+
+            if !play_again {
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    /// The main function for the Wordle game, implement your own logic here
+    pub fn launch(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        // init
+        game::load_game(&mut self.args, &mut self.game_recorder, &mut self.game_data)?;
+
+        self.game_loop()?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,11 @@
-use clap::Parser;
-use std::io;
 mod args;
-use args::Args;
-
-use crate::game::init_game;
 mod builtin_words;
 mod game;
 mod recorder;
 
-fn game_loop(
-    is_tty: bool,
-    args: &mut Args,
-    game_recorder: &mut recorder::GameRecorder,
-    game_data: &mut recorder::GameData,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let mut final_words = Vec::<String>::new();
-    let mut acceptable = Vec::<String>::new();
-    init_game(args, &mut final_words, &mut acceptable)?;
-    loop {
-        game::start_one_game(
-            is_tty,
-            args,
-            game_recorder,
-            &final_words,
-            &acceptable,
-            game_data,
-        );
-        // Show stats if requested
-        if args.stats {
-            game_recorder.print();
-        }
-
-        // Day++
-        if args.day.is_some() {
-            args.day = Some(args.day.unwrap() + 1);
-        }
-
-        // Save game data if requested
-        if args.state.is_some() {
-            game_data.save(&args)?;
-        }
-
-        // Do not play again if word is specified
-        if args.word.is_some() {
-            break;
-        }
-
-        let play_again = loop {
-            let mut buf = String::new();
-            let is_eof = io::stdin().read_line(&mut buf).expect("Invalid input!");
-            if is_eof == 0 {
-                break false;
-            }
-            buf = buf.trim().to_string();
-            if buf == "Y" {
-                break true;
-            } else if buf == "N" {
-                break false;
-            }
-            println!("Invalid input!");
-        };
-
-        if !play_again {
-            break;
-        }
-    }
-    Ok(())
-}
-
-/// The main function for the Wordle game, implement your own logic here
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // init
-    let is_tty = atty::is(atty::Stream::Stdout);
-    let mut args = Args::parse();
-    let mut game_recorder = recorder::GameRecorder::new();
-    let mut game_data = recorder::GameData::new();
-
-    if args.debug {
-        println!("{args:?}");
-    }
-
-    game::load_game(&mut args, &mut game_recorder, &mut game_data)?;
-
-    if args.debug {
-        println!("{args:?}");
-        panic!();
-    }
-
-    game_loop(is_tty, &mut args, &mut game_recorder, &mut game_data)?;
+    let mut wordle_cli = wordle::Wordle::new();
+    wordle_cli.launch()?;
 
     Ok(())
 }
