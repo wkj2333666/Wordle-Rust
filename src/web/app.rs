2622809de3391d7a62ad0f18d0b63ca3409b8dca
wkj2333666 <2695071759@qq.com>
2025-08-29 09:33:28 +0800

    fix: handle losing game fix: rename trunk.toml to Trunk.toml feat: open to external port 8080

--- src/web/app.rs
+++ src/web/app.rs
@@ -16,7 +16,7 @@ pub struct App {
     game: Wordle,
 
     ans: String,
-    win: bool,
+    win: Option<bool>,
     guess_results: wordle_lib::game::Guess,
 }
 
@@ -32,7 +32,7 @@ impl Component for App {
             buffer: GuessInputBuffer::new(),
             game: wordle_lib::Wordle::new(),
             ans: String::new(),
-            win: false,
+            win: None,
             guess_results: wordle_lib::game::Guess::new(),
         };
 
@@ -53,11 +53,19 @@ impl Component for App {
                 <div onkeydown={on_keydown_callback} tabindex="0">
                     { self.board.view() }
                     {
-                        if self.win {
+                        if self.win == Some(true) {
                             html! {
                                 <div class="btn-container">
                                     <button class="game-button" onclick={link.callback(|_| WordleMsg::StartNew)}>
-                                        { "You Win! Click to guess next word!" }
+                                        { "You win! Click to guess next word!" }
+                                    </button>
+                                </div>
+                            }
+                        } else if self.win == Some(false) {
+                            html! {
+                                <div class="btn-container">
+                                    <button class="game-button" onclick={link.callback(|_| WordleMsg::StartNew)}>
+                                        { format!("You lose! The answer was \"{}\". Click to guess next word!", self.answer) }
                                     </button>
                                 </div>
                             }
@@ -73,8 +81,9 @@ impl Component for App {
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            WordleMsg::KeyPress(key) if !self.win => self.get_one_key_input(&key, _ctx),
-            WordleMsg::WinGame => self.win = true,
+            WordleMsg::KeyPress(key) if self.win == None => self.get_one_key_input(&key, _ctx),
+            WordleMsg::WinGame => self.win = Some(true),
+            WordleMsg::LoseGame => self.win = Some(false),
             WordleMsg::StartNew => self.start_new_game(),
             _ => (),
         };
@@ -126,10 +135,14 @@ impl App {
         self.attempts += 1;
         log::debug!("Updated attempts: {}", self.attempts);
 
-        if game_win || self.attempts == 6 {
+        if game_win {
             self.game.game_recorder.add_game(game_win, self.attempts);
             // self.start_new_game();
             link.send_message(WordleMsg::WinGame);
+        } else if self.attempts == 6 {
+            self.game.game_recorder.add_game(game_win, self.attempts);
+            // self.start_new_game();
+            link.send_message(WordleMsg::LoseGame);
         }
 
         self.buffer.clear();
@@ -142,7 +155,7 @@ impl App {
         log::info!("Answer of this game: {}", &self.ans);
         self.game.day_increment();
         self.guess_results.clear();
-        self.win = false;
+        self.win = None;
 
         self.buffer.clear();
         self.attempts = 0;
--- src/web/msg.rs
+++ src/web/msg.rs
@@ -3,5 +3,6 @@ use yew::prelude::*;
 pub enum WordleMsg {
     KeyPress(KeyboardEvent),
     WinGame,
+    LoseGame,
     StartNew,
 }
